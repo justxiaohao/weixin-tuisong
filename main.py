51349d41d3111ffe89bdef29f4dcb05f67810811
justxiaohao <124175105+justxiaohao@users.noreply.github.com>
2023-02-02 10:48:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -195,12 +195,12 @@ def tip():
         today = data["newslist"][0]["date"]
         week = data["newslist"][0]["week"]
         windspeed = data["newslist"][0]["windspeed"]
-        return pop,tips,todays,weeks,windspeed
+        return pop,tips,today,week,windspeed
     else:
         return "",""
 
 #推送信息
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, pop, tips, note_en, note_ch, health_tip, lucky_, todays, weeks, windspeed):
+def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, pop, tips, note_en, note_ch, health_tip, lucky_, today, week, windspeed):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     # year = localtime().tm_year
@@ -367,7 +367,7 @@ if __name__ == "__main__":
     lucky_ = lucky()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, city, weather, max_temperature, min_temperature, pipi, lizhi,pop,tips, note_en, note_ch, health_tip, lucky_)
+        send_message(user, accessToken, city, weather, max_temperature, min_temperature, pipi, lizhi,pop,tips, note_en, note_ch, health_tip, lucky_, today, week, windspeed)
     import time
     time_duration = 3.5
     time.sleep(time_duration)
